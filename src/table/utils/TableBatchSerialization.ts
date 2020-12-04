eea461f6df8fb8febb3d8b6090e588257a58cb0d
edwin-huber <edwin.huber@microsoft.com>
2020-12-04 17:08:05 +0100

    sharing this mess with Martha

--- src/table/utils/TableBatchSerialization.ts
+++ src/table/utils/TableBatchSerialization.ts
@@ -5,19 +5,27 @@ import { BatchType } from "../../common/BatchOperation";
 import BatchSubResponse from "../../common/BatchSubResponse";
 import IBatchSerialization from "../../common/IBatchSerialization";
 
+// The semantics for entity group transactions are defined by the OData Protocol Specification.
+// https://www.odata.org/
+// http://docs.oasis-open.org/odata/odata-json-format/v4.01/odata-json-format-v4.01.html#_Toc38457781
 export class TableBatchSerialization implements IBatchSerialization {
+
+  private batchGuidMatchingRegex = new RegExp("$(--batch_)");
+
   public deserializeBatchRequest(batchRequests: string): BatchOperation[] {
-    /*const HTTP_LINE_ENDING = "\r\n";
-    const HTTP_VERSION_1_1 = "HTTP/1.1";
-    // batch_{​​batchid}​​
+
+
+    const batchGuid = 
     const batchBoundary = `batch_${batchGuid}​​`;
     const changesetBoundary = `changeset_${changesetId}​​`;
-
-    const subRequestPrefix = `--${changesetBoundary}​​${HTTP_LINE_ENDING}​​${HeaderConstants.CONTENT_TYPE}​​: application/http${HTTP_LINE_ENDING}​​${HeaderConstants.CONTENT_TRANSFER_ENCODING}​​: binary`;
     const changesetEnding = `--${changesetBoundary}​​--`;
     const batchEnding = `--${batchBoundary}​​`;
-    */
+    const HTTP_LINE_ENDING = "\r\n";
+    const subRequestPrefix = `--${changesetBoundary}​​${HTTP_LINE_ENDING}​​${HeaderConstants.CONTENT_TYPE}​​: application/http${HTTP_LINE_ENDING}​​${HeaderConstants.CONTENT_TRANSFER_ENCODING}​​: binary`;
+    const splitBody = batchRequests.split(subRequestPrefix);
+
     return [new BatchOperation(BatchType.table, BatchOperationType.unknown)];
+    
   }
 
   // Has default response for now
